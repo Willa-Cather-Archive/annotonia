57ac9e82f7b21524b56ae2af5f19bcd5173c9e96
Greg Tunink <techgique@gmail.com>
2016-05-16 09:50:00 -0500

    Switch to LABjs to more cleanly load Annotonia
    Add LABjs library to clean up the code to add to each HTML page
    Move Annotonia config and CSS loading to annotonia.js
    Load Annotonia JavaScript libraries and annotonia.js with LABjs

---
+++ public/js/annotonia/annotonia.js
@@ -0,0 +1,79 @@
+// Annotator's styling and images
+$('head').append('<link rel="stylesheet" href="js/annotonia/annotator/pkg/annotator.min.css">');
+
+
+// To load only annotations for this specific page
+// Also used by Store plugin below
+var pageID = window.location.pathname.match(/\/([^./]+)\.html$/);
+pageID = pageID[1] ? pageID[1] : undefined;
+
+var optionsOffline = {
+  setAnnotationData: function (note) {
+    note.pageID = pageID;
+  },
+  shouldLoadAnnotation : function (note) {
+    return note.pageID === pageID;
+  }
+}
+
+
+// RichText w TinyMCE
+$('head').append('<link rel="stylesheet" href="js/annotonia/richText-annotator/richText-annotator.min.css">');
+
+// Clean up status bar styling
+$('head').append('<style> \
+  .mce-path { display: none !important; } \
+  .mce-statusbar { border-width: 0 !important; } \
+</style>');
+
+var optionsRichText = {
+  tinymce:{
+    extended_valid_elements : "iframe[src|frameborder|style|scrolling|class|width|height|name|align|id]",
+    menubar: false,
+    plugins: "media image insertdatetime link code",
+    selector: "li.annotator-item textarea",
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | code ",
+    toolbar_items_size: 'small',
+  }
+};
+
+
+// The Store plugin. Saves annotations to a remote backend
+var optionsStore = {
+  annotationData: {
+    'pageID' : pageID
+  },
+  loadFromSearch: {
+    'pageID' : pageID
+  },
+  prefix: 'https://rosie.unl.edu/annostore'
+};
+
+
+// Tags
+$('head').append('<link rel="stylesheet" href="js/annotonia/tags-annotator/build/tags-annotator.min.css">');
+
+var optionsTags = {
+  tag: "Needs Annotated:red,Draft:yellow,Complete:green,Published:blue"
+};
+
+
+
+$(function ($) {
+  if (typeof $.fn.annotator !== 'function') {
+    alert("Annotator was not able to load properly.");
+  } else {
+    // This is the important bit: how to create the annotator and add
+    // plugins
+    $('#airlock')
+      .annotator()
+      .annotator('addPlugin', 'Filter')
+//      .annotator('addPlugin', 'HighlightTags', optionsTags)
+//      .annotator('addPlugin', 'Offline', optionsOffline)
+      .annotator('addPlugin', 'RichText', optionsRichText)
+      .annotator('addPlugin', 'Store', optionsStore)
+      .annotator('addPlugin', 'Unsupported')
+    ;
+  }
+});
+
